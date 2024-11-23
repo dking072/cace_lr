ddef22825d65a4a6bd9718c2d9a77324e9cbd478
dking072 <danielking4812@gmail.com>
2024-11-23 01:18:46 -0600

    first commit

---
+++ cace_lr/data.py
@@ -0,0 +1,110 @@
+import ase
+import os
+import pandas as pd
+import numpy as np
+import torch
+import h5py
+import lightning as L
+
+#loader to import dicts:
+from cace.data.atomic_data import AtomicData
+from cace.tools.torch_geometric import Dataset, DataLoader
+
+def from_h5key(h5key,h5fn,cutoff=None,cell=50):
+    with h5py.File(h5fn, "r") as f:
+        data = f[h5key]
+
+        #Make atoms object
+        els = np.array(data["atomic_numbers"])
+        pos = np.array(data["positions"])
+        pos = pos - pos.mean(axis=0) #center
+        atoms = ase.Atoms(numbers=els,positions=pos)
+        ad = AtomicData.from_atoms(atoms,cutoff=cutoff) #makes graph structure
+
+        ad.energy = torch.Tensor(np.array(data["energy"]))
+        ad.force = torch.Tensor(np.array(data["force"]))
+        ad.dipole = torch.Tensor(np.array(data["dipole"]))[None,:]
+        ad.cell = torch.Tensor([[cell,0,0],[0,cell,0],[0,0,cell]])
+        
+        return ad
+
+class SpiceDataset(Dataset):
+    def __init__(self,root="data/aodata.h5",cutoff=4.0,
+                transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.root = root
+        self.cutoff = cutoff
+
+    def len(self):
+        with h5py.File(self.root, "r") as f:
+            return len(f.keys())
+    
+    def get(self, idx):
+        return from_h5key(f"c{idx}",h5fn=self.root,cutoff=self.cutoff)
+
+class SpiceInMemoryDataset(Dataset):
+    def __init__(self,root="data/aodata.h5",cutoff=4.0,
+                transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, transform, pre_transform, pre_filter)
+        self.root = root
+        self.cutoff = cutoff
+        self.prepare_data()
+
+    def get_h5(self,i):
+        return from_h5key(f"c{i}",h5fn=self.root,cutoff=self.cutoff)
+    
+    def prepare_data(self):
+        with h5py.File(self.root, "r") as f:
+            data_len = len(f.keys())
+        self.dataset = [self.get_h5(i) for i in range(data_len)]
+
+    def len(self):
+        return len(self.dataset)
+    
+    def get(self, idx):
+        return self.dataset[idx]
+
+class SpiceData(L.LightningDataModule):
+    def __init__(self, root="data/aodata.h5", cutoff=4.0, in_memory=False, drop_last=True,
+                 batch_size=32, valid_p=0.1, test_p=0.1):
+        super().__init__()
+        self.batch_size = batch_size
+        self.root = root
+        self.valid_p = valid_p
+        self.test_p = test_p
+        self.cutoff = cutoff
+        self.drop_last = drop_last
+        self.in_memory = in_memory
+        try:
+            self.num_cpus = int(os.environ['SLURM_JOB_CPUS_PER_NODE'])
+        except:
+            self.num_cpus = os.cpu_count()
+        self.prepare_data()
+    
+    def prepare_data(self):
+        if not self.in_memory:
+            dataset = SpiceDataset(self.root,cutoff=self.cutoff)
+        else:
+            dataset = SpiceInMemoryDataset(self.root,cutoff=self.cutoff)
+        torch.manual_seed(12345)
+        dataset = dataset.shuffle()
+        cut1 = int(len(dataset)*(1-self.valid_p-self.test_p))
+        cut2 = int(len(dataset)*(1-self.test_p))
+        self.train = dataset[:cut1]
+        self.val = dataset[cut1:cut2]
+        self.test = dataset[cut2:]
+
+    def train_dataloader(self):
+        train_loader = DataLoader(self.train, batch_size=self.batch_size, drop_last=self.drop_last,
+                                  shuffle=True, num_workers = self.num_cpus)
+        return train_loader
+
+    def val_dataloader(self):
+        val_loader = DataLoader(self.val, batch_size=self.batch_size, drop_last=False,
+                                shuffle=False, num_workers = self.num_cpus)
+        return val_loader
+
+    def test_dataloader(self):
+        val_loader = DataLoader(self.test, batch_size=self.batch_size, drop_last=False,
+                                shuffle=False, num_workers = self.num_cpus)
+        return val_loader
---
+++ model_runs/cace_dipep_sr.py
@@ -0,0 +1,176 @@
+import os
+import glob
+import torch
+from cace.tasks import LightningData, LightningTrainingTask
+
+on_cluster = False
+if 'SLURM_JOB_CPUS_PER_NODE' in os.environ.keys():
+    on_cluster = True
+if on_cluster:
+    root = None #edit here later
+else:
+    root = "/home/king1305/ORBITAL_LABELING/cace_lr/data/spice-dipep.h5"
+
+LR = False
+logs_name = "cacesr_dipep"
+if LR:
+    logs_name += "_lr"
+else:
+    logs_name += "_sr"
+cutoff = 4.0
+batch_size = 4
+from cace_lr.data import SpiceData
+data = SpiceData(root,cutoff,batch_size=batch_size,in_memory=True)
+
+from cace.representations import Cace
+from cace.modules import BesselRBF, GaussianRBF, GaussianRBFCentered
+from cace.modules import PolynomialCutoff
+
+#Model
+radial_basis = BesselRBF(cutoff=cutoff, n_rbf=6, trainable=True)
+cutoff_fn = PolynomialCutoff(cutoff=cutoff)
+
+representation = Cace(
+    zs=[1,6,7,8],
+    n_atom_basis=4,
+    embed_receiver_nodes=True,
+    cutoff=cutoff,
+    cutoff_fn=cutoff_fn,
+    radial_basis=radial_basis,
+    n_radial_basis=12,
+    max_l=4,
+    max_nu=3,
+    num_message_passing=1,
+    type_message_passing=["M", "Ar", "Bchi"],
+    args_message_passing={'Bchi': {'shared_channels': False, 'shared_l': False}},
+    timeit=False
+)
+
+for batch in data.train_dataloader():
+    exdatabatch = batch
+    break
+
+import cace
+from cace.models import NeuralNetworkPotential
+from cace.modules import Atomwise, Forces
+
+atomwise = Atomwise(n_layers=3,
+                    output_key="pred_energy",
+                    n_hidden=[32,16],
+                    n_out=1,
+                    use_batchnorm=False,
+                    add_linear_nn=True)
+
+forces = Forces(energy_key="pred_energy",
+                forces_key="pred_force")
+
+model = NeuralNetworkPotential(
+    input_modules=None,
+    representation=representation,
+    output_modules=[atomwise,forces]
+)
+
+if LR:
+    q = cace.modules.Atomwise(
+        n_layers=3,
+        n_hidden=[24,12],
+        n_out=1,
+        per_atom_output_key='q',
+        output_key = 'tot_q',
+        residual=False,
+        add_linear_nn=True,
+        bias=False)
+    
+    ep = cace.modules.EwaldPotential(dl=3,
+                        sigma=1.5,
+                        feature_key='q',
+                        output_key='ewald_potential',
+                        remove_self_interaction=False,
+                       aggregation_mode='sum')
+    
+    forces_lr = cace.modules.Forces(energy_key='ewald_potential',
+                                        forces_key='ewald_forces')
+    
+    cace_nnp_lr = NeuralNetworkPotential(
+        input_modules=None,
+        representation=representation,
+        output_modules=[q, ep, forces_lr]
+    )
+
+    pot1 = {'pred_energy': 'pred_energy', 
+            'pred_force': 'pred_force',
+            'weight': 1,
+           }
+    
+    pot2 = {'pred_energy': 'ewald_potential', 
+            'pred_force': 'ewald_forces',
+            'weight': 1,
+           }
+    
+    model = cace.models.CombinePotential([model, cace_nnp_lr], [pot1,pot2])
+
+#Losses
+from cace.tasks import GetLoss
+e_loss = GetLoss(
+    target_name="energy",
+    predict_name='pred_energy',
+    loss_fn=torch.nn.MSELoss(),
+    loss_weight=1,
+)
+f_loss = GetLoss(
+    target_name="force",
+    predict_name='pred_force',
+    loss_fn=torch.nn.MSELoss(),
+    loss_weight=1000,
+)
+losses = [e_loss,f_loss]
+
+#Metrics
+from cace.tools import Metrics
+e_metric = Metrics(
+            target_name="energy",
+            predict_name='pred_energy',
+            name='e',
+            metric_keys=["rmse"],
+            per_atom=True,
+        )
+f_metric = Metrics(
+            target_name="force",
+            predict_name='pred_force',
+            metric_keys=["rmse"],
+            name='f',
+        )
+metrics = [e_metric,f_metric]
+
+#Init lazy layers
+for batch in data.train_dataloader():
+    exdatabatch = batch
+    break
+model(exdatabatch)
+
+#Check for checkpoint and restart if found:
+chkpt = None
+dev_run = False
+if os.path.isdir(f"lightning_logs/{logs_name}"):
+    latest_version = None
+    num = 0
+    while os.path.isdir(f"lightning_logs/{logs_name}/version_{num}"):
+        latest_version = f"lightning_logs/{logs_name}/version_{num}"
+        num += 1
+    if latest_version:
+        chkpt = glob.glob(f"{latest_version}/checkpoints/*.ckpt")[0]
+if chkpt:
+    print("Checkpoint found!",chkpt)
+    print("Restarting...")
+    dev_run = False
+
+progress_bar = True
+if on_cluster:
+    torch.set_float32_matmul_precision('medium')
+    progress_bar = False
+task = LightningTrainingTask(model,losses=losses,metrics=metrics,
+                             logs_directory="lightning_logs",name=logs_name,
+                             scheduler_args={'mode': 'min', 'factor': 0.8, 'patience': 10},
+                             optimizer_args={'lr': 0.01},
+                            )
+task.fit(data,dev_run=dev_run,max_epochs=500,chkpt=chkpt,progress_bar=progress_bar)
